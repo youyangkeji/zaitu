f1729f2dd1bbb2262e1b67f7006c43f74541c50f
WMG <1072675504@qq.com>
2019-07-16 19:32:36 +0800

    Initial commit

---
+++ src/main/java/cn/hfyouyang/zaitu/ZaituApplication.java
@@ -0,0 +1,13 @@
+package cn.hfyouyang.zaitu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaituApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZaituApplication.class, args);
+    }
+
+}
